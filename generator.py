6b659d2a3bb65d0ba6b1549d01214993b087e98b
asama <asamahayder@yahoo.dk>
2023-04-23 13:40:34 +0200

    Clean up

--- generator.py
+++ generator.py
@@ -1,31 +1,3 @@
-import json.encoder
-import time
-import urllib.request
-import urllib.parse
-
-"""while True:
-    d = {"value": 5}
-    data = urllib.parse.urlencode(d).encode("utf-8")
-    req = urllib.request.Request("http://localhost:8081/randomInt")
-    req.add_header("Content-type", "application/json")
-    urllib.request.urlopen(req, data).read()
-
-    time.sleep(2000)"""
-
-"""import urllib.request
-import json
-
-reqUrl = 'http://localhost:8081/randomInt'
-reqData = {"value": 5}
-reqData = json.encoder(reqData)
-headers = {
-    'Content-Type': 'application/json'
-}
-method = 'POST'
-req = urllib.request.Request(reqUrl, reqData, headers, method=method)
-with urllib.request.urlopen(req) as res:
-    body = json.load(res)"""
-
 import requests
 import time
 import random
