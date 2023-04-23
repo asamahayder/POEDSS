9b92dd24f0144711c64a1dd56fb27123c953023a
asama <asamahayder@yahoo.dk>
2023-04-23 13:50:49 +0200

    testing

--- generator.py
+++ generator.py
@@ -2,11 +2,17 @@ import requests
 import time
 import random
 
-while True:
+"""while True:
     number = random.randint(0, 9)
     r = requests.post('http://localhost:8081/randomInt', json={
         "value": number
     })
     print(f"Status Code: {r.status_code}, Response: {r.url}")
-    time.sleep(2)
+    time.sleep(2)"""
+
+
+r = requests.post('http://localhost:8600/card-status', json={
+    "status": "Success"
+})
+print(f"Status Code: {r.status_code}, Response: {r.url}")
 
